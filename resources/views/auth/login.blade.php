f47fc97b63b84825f70ecbb2d2c5e04f6f0ba207
shxpe <qwezxasd1@yandex.ru>
2025-12-17 04:26:31 +0400

    Замена x-text-input на x-password-input для поля пароля

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,10 +22,9 @@
 
         <div class="mt-4">
             <x-input-label for="password" value="Пароль" />
-            <x-text-input
+            <x-password-input
                 id="password"
                 class="block mt-1 w-full"
-                type="password"
                 name="password"
                 required
                 autocomplete="current-password"
