97c852d86782efd1fc9759d28a02f0d26acfa4c5
shxpe <qwezxasd1@yandex.ru>
2025-12-18 13:16:04 +0400

    feat: add Filament admin panel configuration

---
+++ config/filament.php
@@ -0,0 +1,147 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filament Path
+    |--------------------------------------------------------------------------
+    |
+    | The default is `admin` but you can change it to whatever works best and
+    | doesn't conflict with the routing in your application.
+    |
+    */
+
+    'path' => env('FILAMENT_PATH', 'admin'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filament Core Path
+    |--------------------------------------------------------------------------
+    |
+    | This is the path which Filament will use to load its core routes and assets.
+    | You may change it if it conflicts with your other routes.
+    |
+    */
+
+    'core_path' => env('FILAMENT_CORE_PATH', 'filament'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filament Domain
+    |--------------------------------------------------------------------------
+    |
+    | You may change the domain where Filament should be active. If the domain
+    | is empty, all domains will be valid.
+    |
+    */
+
+    'domain' => env('FILAMENT_DOMAIN'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Homepage URL
+    |--------------------------------------------------------------------------
+    |
+    | This is the URL that Filament will redirect the user to when they click
+    | on the sidebar's header.
+    |
+    */
+
+    'home_url' => '/',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Brand Name
+    |--------------------------------------------------------------------------
+    |
+    | This will be displayed on the login page and in the sidebar's header.
+    |
+    */
+
+    'brand' => 'Perminov Store',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Auth
+    |--------------------------------------------------------------------------
+    |
+    | This is the authentication guard Filament will use to authenticate users.
+    |
+    */
+
+    'auth' => [
+        'guard' => env('FILAMENT_AUTH_GUARD', 'web'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pages
+    |--------------------------------------------------------------------------
+    |
+    | This is the namespace and directory that Filament will automatically
+    | register pages from. You may also register pages here.
+    |
+    */
+
+    'pages' => [
+        'namespace' => 'App\\Filament\\Pages',
+        'path' => app_path('Filament/Pages'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Resources
+    |--------------------------------------------------------------------------
+    |
+    | This is the namespace and directory that Filament will automatically
+    | register resources from. You may also register resources here.
+    |
+    */
+
+    'resources' => [
+        'namespace' => 'App\\Filament\\Resources',
+        'path' => app_path('Filament/Resources'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Widgets
+    |--------------------------------------------------------------------------
+    |
+    | This is the namespace and directory that Filament will automatically
+    | register dashboard widgets from. You may also register widgets here.
+    |
+    */
+
+    'widgets' => [
+        'namespace' => 'App\\Filament\\Widgets',
+        'path' => app_path('Filament/Widgets'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Middleware
+    |--------------------------------------------------------------------------
+    |
+    | You may customize the middleware stack that Filament uses to handle
+    | requests.
+    |
+    */
+
+    'middleware' => [
+        'auth' => [
+            'web',
+        ],
+        'base' => [
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Filament\Http\Middleware\DispatchServingFilamentEvent::class,
+        ],
+    ],
+
+];
