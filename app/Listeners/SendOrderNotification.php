cb55a5f52084760ef47e750b20ba16b36fcca66f
shxpe <qwezxasd1@yandex.ru>
2025-12-18 11:12:22 +0400

    Feature: добавлены Listeners для уведомлений о заказах

--- app/Listeners/SendOrderNotification.php
+++ app/Listeners/SendOrderNotification.php
@@ -12,18 +12,30 @@ class SendOrderNotification implements ShouldQueue
     use InteractsWithQueue;
 
     /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
      * Handle the event.
      */
     public function handle(OrderCreated $event): void
     {
-        $order = $event->order;
+        // Заглушка для отправки email
+        // В будущем можно интегрировать Mail::to($event->order->user->email)
+        
+        Log::info('Отправлено уведомление о создании заказа', [
+            'order_id' => $event->order->id,
+            'user_id' => $event->order->user_id,
+            'user_email' => $event->order->user->email,
+            'total_price' => $event->order->total_price,
+        ]);
 
         // TODO: Реализовать отправку email
-        // Mail::to($order->user->email)->send(new OrderCreatedMail($order));
-
-        Log::info('Отправлено уведомление о создании заказа', [
-            'order_id' => $order->id,
-            'user_id' => $order->user_id,
-        ]);
+        // Mail::to($event->order->user->email)
+        //     ->send(new OrderCreatedMail($event->order));
     }
 }
