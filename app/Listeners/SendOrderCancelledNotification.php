95f06ac5bf60f4aeddc2503a8155cbebcaa1ccd0
shxpe <qwezxasd1@yandex.ru>
2025-12-18 11:41:50 +0400

    Refactor: убран ShouldQueue из SendOrderCancelledNotification

--- app/Listeners/SendOrderCancelledNotification.php
+++ app/Listeners/SendOrderCancelledNotification.php
@@ -3,14 +3,10 @@
 namespace App\Listeners;
 
 use App\Events\OrderCancelled;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 
-class SendOrderCancelledNotification implements ShouldQueue
+class SendOrderCancelledNotification
 {
-    use InteractsWithQueue;
-
     /**
      * Create the event listener.
      */
@@ -24,18 +20,17 @@ class SendOrderCancelledNotification implements ShouldQueue
      */
     public function handle(OrderCancelled $event): void
     {
-        // Заглушка для отправки email
-        // В будущем можно интегрировать Mail::to($event->order->user->email)
-        
-        Log::info('Отправлено уведомление об отмене заказа', [
+        // Логирование отмены заказа
+        Log::info('Заказ отменён - уведомление', [
             'order_id' => $event->order->id,
             'user_id' => $event->order->user_id,
-            'user_email' => $event->order->user->email,
+            'user_name' => $event->order->user->name,
             'status' => $event->order->status,
+            'total_price' => $event->order->total_price,
         ]);
 
-        // TODO: Реализовать отправку email
-        // Mail::to($event->order->user->email)
-        //     ->send(new OrderCancelledMail($event->order));
+        // Для учебного проекта достаточно логирования
+        // В продакшене здесь была бы отправка email:
+        // Mail::to($event->order->user->email)->send(new OrderCancelledMail($event->order));
     }
 }
