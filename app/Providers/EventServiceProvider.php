812057575e20d6ec8df259b12e81d8ce018864e1
shxpe <qwezxasd1@yandex.ru>
2025-12-18 11:17:45 +0400

    Feature: создан EventServiceProvider с регистрацией всех событий

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use App\Events\OrderCreated;
+use App\Events\OrderCancelled;
+use App\Events\ReviewCreated;
+use App\Listeners\SendOrderNotification;
+use App\Listeners\SendOrderCancelledNotification;
+use App\Listeners\UpdateProductRatingCache;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event to listener mappings for the application.
+     *
+     * @var array<class-string, array<int, class-string>>
+     */
+    protected $listen = [
+        OrderCreated::class => [
+            SendOrderNotification::class,
+        ],
+        OrderCancelled::class => [
+            SendOrderCancelledNotification::class,
+        ],
+        ReviewCreated::class => [
+            UpdateProductRatingCache::class,
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Determine if events and listeners should be automatically discovered.
+     */
+    public function shouldDiscoverEvents(): bool
+    {
+        return false;
+    }
+}
