b48c1c527d443830ff1bb1e848464fefa1177796
shxpe <qwezxasd1@yandex.ru>
2025-12-18 11:59:04 +0400

    Refactor: убран ShouldQueue из UpdateProductRatingCache

--- app/Listeners/UpdateProductRatingCache.php
+++ app/Listeners/UpdateProductRatingCache.php
@@ -4,8 +4,6 @@ namespace App\Listeners;
 
 use App\Events\ReviewCreated;
 use App\Services\ProductService;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 
 class UpdateProductRatingCache
 {
